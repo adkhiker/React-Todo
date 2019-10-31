5d77d483d6929806f65c283156135752bd6194ca
adkhiker <poirier.greg@gmail.com>
2019-10-30 23:20:40 -0400

    Persistant data, styling and search added

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const TodoForm = (props) => {
-  
-
-
- 
-
-   
-        return (
-            <form onSubmit={props.addTask}>
-                <input onChange={props.inputHandler} value={props.task} placeholder='Add Task' />
-                <button>Submit New Task</button>
-            </form>
-        )
-    
-}
+const TodoForm = props => {
+  return (
+    <form onSubmit={props.addTask}>
+      <input
+        onChange={props.inputHandler}
+        value={props.task}
+        placeholder="Add Task"
+      />
+      <button>Submit New Task</button>
+    </form>
+  );
+};
 
 export default TodoForm;
